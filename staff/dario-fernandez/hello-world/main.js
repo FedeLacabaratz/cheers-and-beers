85b4589117ef71921d918e3c92b86987a506f042
Darío Fernández <dariofedes@protonmail.com>
2020-01-14 16:49:54 +0100

    add my staff

---
+++ staff/dario-fernandez/hello-world/main.js
@@ -0,0 +1,7 @@
+document.getElementsByTagName('h1')[0].addEventListener('click', function() {
+    alert('Hello, World!')
+})
+
+document.getElementsByTagName("button")[0].addEventListener('click', function() {
+    alert('Caguen p#$!*$!');
+})
