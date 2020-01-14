bd5634c545e9b31e1078e925a46003409cb89d13
Federico Lacabaratz <federicolacabaratz@MacBook-Pro-de-Federico.local>
2020-01-14 16:50:01 +0100

    add my staff

---
+++ staff/federico-lacabaratz/hello-world/main.js
@@ -0,0 +1,6 @@
+document.getElementsByTagName('h1')[0].addEventListener('click', function() {
+    alert('Hello, World!')
+});
+document.getElementsByTagName('button')[0].addEventListener('click', function() {
+    alert('Soy un button \nNo me gusta este color :(')
+})
