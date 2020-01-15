076d9bdbaec737c7e1317d883a68ecffde33e08c
kike4815 <e.pedros.sanchez@gmail.com>
2020-01-15 10:04:12 +0100

    dom-tree

---
+++ staff/enric-pedros/dom-tree/index.js
@@ -0,0 +1,7 @@
+var tags = document.querySelectorAll('*')
+var tagsUpdated = Array.prototype.map.call(tags, function(tag) {
+  tag.classList = []
+  tag.id = ""
+  return tag
+})
+console.log(tagsUpdated)
