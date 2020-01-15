f3a5df868619757f998d1536eaf5ae99480ab96f
RPXIC <rpc2works@gmail.com>
2020-01-15 15:39:54 +0100

    1

--- staff/ruben-ponce/dom-tree/index.js
+++ staff/ruben-ponce/dom-tree/index.js
@@ -1,23 +1,23 @@
-var element = document.children;
-
-function html (elements) {
+function isSemantic(element) {
+    return semantic.indexOf(element.tagName.toLowerCase()) > -1;
+}
+var semantic = ['html','body','head','header','article','aside','details','figcaption','figure','footer','main','nav','section','summary','time','mark'];
+function html(elements, indent) {
     var code = '';
-
+    if (indent === undefined) indent = '';
     for (var i = 0; i < elements.length; i++) {
         var element = elements[i];
-
-        code += '<' + element.tagName + '>';
-
-        if(element.children.length) {
-            
-            code += html(element.children)
-
+        if (isSemantic(element)) {
+            code += indent + '<' + element.tagName + '>\n';
+            if (element.children.length) {
+                code += html(element.children, indent + '\t');
+            }
+            code += indent + '</' + element.tagName + '>\n';
+        } else {
+            code += html(element.children, indent + '\t');
         }
-
-        code += '</' + element.tagName + '>';
-        
     }
-    return code;semantic-
+    return code;
 }
 console.log(html(document.children));
 
