c7dc33f6dc20e8acdf8793e64dc245fbe87c0b9e
Eric Aig <>
2020-01-15 14:47:50 +0100

    dom-tree

--- staff/bamidele-aighewi/dom-tree/index.js
+++ staff/bamidele-aighewi/dom-tree/index.js
@@ -1,28 +1,106 @@
-function tabulation(total) {
-    var output = '';
-    for (var x = 0; x < total; x++) output += '-';
-    return output;
-}
+function html(elements) {
+    var code = '';
 
-var level = 1;
-function getDomTree(docChildren, identation) {
-    var tagNames = '';
-    for(var x = 0; x < docChildren.length; x++){
-        var child = docChildren[x];
-        var tagName = child.tagName;
+    for (var i = 0; i < elements.length; i++) {
+        var element = elements[i];
 
-        tagNames += '<' + tagName + '>';
+        code += '<' + element.tagName + '>';
 
-        if (child.children.length) {
-            tagNames += getDomTree(child.children, identation);
+        if (element.children.length) {
+            code += html(element.children);
         }
 
-        tagNames += '</' + tagName + '>';
-        level++;
+        code += '</' + element.tagName + '>';
     }
 
-    return tagNames;
+    return code;
 }
 
-var elements = getDomTree(document.children, '');
-console.log(elements);
+console.log(html(document.children));
+function html(elements) {
+    var code = '';
+
+    for (var i = 0; i < elements.length; i++) {
+        var element = elements[i];
+
+        code += '<' + element.tagName + '>';
+
+        if (element.children.length) {
+            code += html(element.children);
+        }
+
+        code += '</' + element.tagName + '>';
+    }
+
+    return code;
+}
+
+console.log(html(document.children));
+
+// 2
+
+function html(elements, indent) {
+    var code = '';
+
+    if (indent === undefined) indent = '';
+
+    for (var i = 0; i < elements.length; i++) {
+        var element = elements[i];
+
+        code += indent + '<' + element.tagName + '>\n';
+
+        if (element.children.length) {
+            code += html(element.children, indent + '\t');
+        }
+
+        code += indent + '</' + element.tagName + '>\n';
+    }
+
+    return code;
+}
+
+console.log(html(document.children));
+
+// 3
+
+function html(elements, indent) {
+    var code = '';
+
+    if (indent === undefined) indent = '';
+
+    for (var i = 0; i < elements.length; i++) {
+        var element = elements[i];
+
+        if (isInline(element))
+            code += (isInline(element.parentElement) ? '' : indent) + '<' + element.tagName + '>';
+        else if (isSelfClosed(element))
+            code += indent + '<' + element.tagName + '/>\n';
+        else
+            code += indent + '<' + element.tagName + '>\n';
+
+        if (element.children.length) {
+            code += html(element.children, indent + '\t');
+        }
+
+        if (isInline(element))
+            code += '</' + element.tagName + '>' + (isInline(element.parentElement) ? '' : '\n');
+        else if (!isSelfClosed(element))
+            code += indent + '</' + element.tagName + '>\n';
+    }
+
+    return code;
+}
+
+var inline = ['h1', 'h2', 'h3', 'h5', 'span', 'time', 'title', 'strong', 'td', 'li', 'button', 'a']; // TODO include more tags
+
+function isInline(element) {
+    return inline.indexOf(element.tagName.toLowerCase()) > -1;
+}
+
+var selfClosed = ['input', 'link', 'br', 'img', 'source']; // TODO include more tags
+
+function isSelfClosed(element) {
+    return selfClosed.indexOf(element.tagName.toLowerCase()) > -1;
+}
+
+console.log(html(document.children));
