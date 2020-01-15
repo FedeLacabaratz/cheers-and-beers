54e35bbb8b476b2d0efd8147f7967eb1c2e8a34e
RPXIC <rpc2works@gmail.com>
2020-01-15 12:33:56 +0100

    v1

---
+++ staff/ruben-ponce/dom-tree/index.js
@@ -0,0 +1,24 @@
+var element = document.children;
+
+function html (elements) {
+    var code = '';
+
+    for (var i = 0; i < elements.length; i++) {
+        var element = elements[i];
+
+        code += '<' + element.tagName + '>';
+
+        if(element.children.length) {
+            
+            code += html(element.children)
+
+        }
+
+        code += '</' + element.tagName + '>';
+        
+    }
+    return code;
+
+}
+console.log(html(document.children));
+
