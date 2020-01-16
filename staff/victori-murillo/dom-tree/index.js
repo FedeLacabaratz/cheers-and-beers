0b78b24f4278d447cf00ba85399ddefcd084eeec
victoriCrediday <victori.murillo.mora@gmail.com>
2020-01-16 13:20:05 +0100

    files js with recursive

--- staff/victori-murillo/dom-tree/index.js
+++ staff/victori-murillo/dom-tree/index.js
@@ -22,3 +22,28 @@ function html(elements) {
 }
 
 console.log(html(document.children))
+
+
+//2
+
+function html(elements, indent) {
+  var code = '';
+
+  if (indent === undefined) indent = '';
+
+  for (var i = 0; i < elements.length; i++) {
+      var element = elements[i];
+
+      code += indent + '<' + element.tagName + '>\n';
+
+      if (element.children.length) {
+          code += html(element.children, indent + '\t');
+      }
+
+      code += indent + '</' + element.tagName + '>\n';
+  }
+
+  return code;
+}
+
+console.log(html(document.children));
