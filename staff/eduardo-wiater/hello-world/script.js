e76b78ba6d595447531e70ba46afe095e96a6cb8
Pocholo77 <kpooh7766@gmail.com>
2020-01-17 09:14:44 +0100

    wrong folders fixed

--- staff/eduardo-wiater/hello-world/script.js
+++
@@ -1,9 +0,0 @@
-document.getElementsByTagName('h1')[0].addEventListener('click',function () { 
-    alert("hello world");
-});
-
-var phrases = ["las rosas son rosas", "el cielo es azul", "no hay nadie mas feo que tu"]
-document.getElementById("pink").addEventListener('click',function(){
-    var num =Math.floor(Math.random()*3)
-    alert(phrases[num])
-})
