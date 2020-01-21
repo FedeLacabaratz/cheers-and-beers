d4c9a67a882cf48b644bd5b4fe25a4d7a013e462
Manuel Barzi <manuelbarzi@gmail.com>
2020-01-21 16:54:41 +0100

    update arrays tests

--- staff/manuel-barzi/arrays/for-each.test.js
+++ staff/manuel-barzi/arrays/for-each.test.js
@@ -2,14 +2,14 @@
 
 console.log('TEST forEach');
 
-console.log('should array be modified adding 10 to each value');
+console.log('should array [1, 2, 3] be modified adding 10 to each value');
 var a = [1, 2, 3];
 forEach(a, function(value, index) { a[index] = value + 10 });
 a.forEach(function(value, index) {
     console.assert(value === index + 1 + 10, 'should value at index ' + index + ' be ' + (index + 1 + 10));
 })
 
-console.log('should each value be added 10 and printed out');
+console.log('should each value of array [1, 2, 3] be added 10 and stored in results array');
 var a = [1, 2, 3];
 var results = [];
 forEach(a, function(value, index) { results[index] = value + 10 });
