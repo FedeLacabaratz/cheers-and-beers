cd2ccdc47c129fadcd100ce6df90ead66c51c91a
MarcSalasTorras <marc.salas.torras@gmail.com>
2020-01-21 15:21:59 +0100

    + flat

---
+++ staff/marc-salas/arrays/flat.js
@@ -0,0 +1,17 @@
+'use strict';
+var arr = [1,2,[3,4,[5,6,[7,8[9,10]]]], {}];
+
+function flat(array, deep , acumulator) {
+    deep  === undefined? deep =1 : null;
+    var result = acumulator || [];
+    for (var i =0; i < array.length; i++){
+        debugger
+        if (array[i] instanceof Array && deep > 0){
+            deep --
+            flat(array[i], deep, result)
+        } else {
+            result[result.length] = array[i];
+        }
+    }
+    return result;
+}
