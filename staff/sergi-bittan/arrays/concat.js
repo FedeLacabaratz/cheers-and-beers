32dceaed1ae2abce55df4051f176ee33525eae07
Sergi <sergibittangonzalez@MacBook-Pro-de-Sergi.local>
2020-01-22 18:11:15 +0100

    retoques

--- staff/sergi-bittan/arrays/concat.js
+++ staff/sergi-bittan/arrays/concat.js
@@ -1,17 +1,23 @@
 "use strict"
 
-//arr.includes(searchElement[, fromIndex])
 
-var result = [1,4,5,7,4,3,6,2,3,4,];
+
+var arr = [1,4,5,7,4,3,6,2,3,4,];
 var arr2 = [1,5,8];
 
+var newArr = [];
 function concat(arr,arr2)
 {
-    for (var i = 0; i<arr2.length; i++)
+    
+    for (var i = 0; i<arr.length; i++)
     {
-        arr[arr.length] = arr2[i];
+        newArr[newArr.length] = arr[i];
     }
-    return arr;
+    for (var j = 0; j<arr2.length; j++)
+    {
+        newArr[newArr.length] = arr2[j];
+    }
+    return newArr;
 };
 
 //concat(arr,arr2);
