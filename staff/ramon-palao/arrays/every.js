450c4aa359c4f89a375119a1d51d1199e2393a4e
ramon <ramon.palao@gmail.com>
2020-01-23 00:55:50 +0100

    add every method test

--- staff/ramon-palao/arrays/every.js
+++ staff/ramon-palao/arrays/every.js
@@ -1,6 +1,8 @@
 var array1= [3, 8, 25, 30, 90, 12, 111];
 
 function every(a, expression){
+    if(!(a instanceof Array)) throw new TypeError(a + " is not an Array at every");
+    if(typeof expression !== "function") throw new TypeError(expression + " is not a function");
     for (var i=0; i<a.length; i++){
         if (expression(a[i]) == false){
             return false;
@@ -8,5 +10,5 @@ function every(a, expression){
     }
     return true;
 }
-var c = every(array1, function(value){return value>0});
-console.log(c);
+//var c = every(array1, function(value){return value>0});
+//console.log(c);
--- staff/ramon-palao/arrays/every.test.js
+++ staff/ramon-palao/arrays/every.test.js
@@ -17,10 +17,47 @@ function it(should, test) {
 function describe(description, tests) {
     'use strict';
 
-    console.log('%c' + description, 'color: blue;');
+    console.log('%c' + "TEST " + description, 'color: blue;');
 
     tests();
 }
 
 // TESTING
 
+describe("every", function(){
+    //Happy Path
+    it("should return true because all the elements from the array are bigger than 2", function(){
+        var ourArray = [10, 8, 25, 30, 90, 12, 11];
+
+        every(ourArray, function(value){return value > 2});
+        ourArray.forEach(function(value, index){
+            assert(value > 2, "should be bigger than 20 but I got value " + value + " in index " + index);
+        })
+    })
+    it ("should return a false element because not all the elements are bigger than 30", function(){
+        var ourArray = [10, 8, 25, 30, 90, 12, 11];
+
+        every(ourArray, function(value){ return value > 30})
+        ourArray.forEach(function(value, index){
+            assert((value[index] > 30) === false, "should return false but I got value " + value + " in index " + index + " and it is bigger than 30")
+        })
+    })
+
+    //Error Handing
+    it("should fail because the first parameter is not an array"), function(){
+
+        (function(){
+            //var ourArray = [10, 8, 25, 30, 90, 12, 11];
+            var _error;
+
+            try {
+                every(function(){});
+            } catch (error) {
+                _error = error;
+            }
+
+            assert(_error instanceof TypeError, "should error be a type of TypeError");
+            assert(_error.message === "function(){} is not an Array at every", "should fail with message 'function(){} is not an Array at every'");
+        })()
+    }
+})
