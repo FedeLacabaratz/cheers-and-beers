428d7c5823eaafb1d40a68606fb7597070ce7b2c
Manuel Barzi <manuelbarzi@gmail.com>
2020-01-22 10:39:12 +0100

    update arrays

--- staff/manuel-barzi/arrays/for-each.test.js
+++ staff/manuel-barzi/arrays/for-each.test.js
@@ -3,16 +3,16 @@
 console.log('TEST forEach');
 
 console.log('should array [1, 2, 3] be modified adding 10 to each value');
-var a = [1, 2, 3];
-forEach(a, function(value, index) { a[index] = value + 10 });
-a.forEach(function(value, index) {
+var array = [1, 2, 3];
+forEach(array, function(value, index) { array[index] = value + 10 });
+array.forEach(function(value, index) {
     console.assert(value === index + 1 + 10, 'should value at index ' + index + ' be ' + (index + 1 + 10));
 })
 
 console.log('should each value of array [1, 2, 3] be added 10 and stored in results array');
-var a = [1, 2, 3];
+var array = [1, 2, 3];
 var results = [];
-forEach(a, function(value, index) { results[index] = value + 10 });
+forEach(array, function(value, index) { results[index] = value + 10 });
 results.forEach(function(result, index) {
-    console.assert(result === a[index] + 10, 'should value at index ' + index + ' be ' + (a[index] + 10));
+    console.assert(result === array[index] + 10, 'should value at index ' + index + ' be ' + (array[index] + 10));
 });
--- staff/manuel-barzi/arrays/push.demo.js
+++ staff/manuel-barzi/arrays/push.demo.js
@@ -2,7 +2,7 @@
 
 console.log('DEMO push');
 
-var a = [1, 2 , 3];
-console.log('it', a);
-push(a, 4);
-console.log('should have added 4 at the end', a);
+var array = [1, 2 , 3];
+console.log('it', array);
+push(array, 4);
+console.log('should have added 4 at the end', array);
