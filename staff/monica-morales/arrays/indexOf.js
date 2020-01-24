4693225ad2db054e47ba6a26d0fe42e889cb0a99
momorales <monimoralesr77@gmail.com>
2020-01-24 19:03:04 +0100

    update

--- staff/monica-morales/arrays/indexOf.js
+++ staff/monica-morales/arrays/indexOf.js
@@ -1,6 +1,7 @@
 'use strict';
 
 function indexOf(arr, value){
+    if(arr === undefined || value === undefined) throw new TypeError('Is necesary put an array'); 
     for( var i = 0; i<arr.length; i++){
         if(arr[i] === value){
         return i;
--- staff/monica-morales/arrays/map.test.js
+++ staff/monica-morales/arrays/map.test.js
@@ -1,4 +1,4 @@
-'use strict';
+/*'use strict';
 
 console.log('TEST map');
 
@@ -41,4 +41,57 @@ try {
     _error = error; 
 }
     console.assert(_error instanceof TypeError, 'should error be of type TypeError');
-    console.assert(_error.message === 'is not expression', 'should fail with message "not exist an expression"');
+    console.assert(_error.message === 'is not expression', 'should fail with message "not exist an expression"');*/
+
+describe('map', function(){
+    it('Original array should be modified applying an expression', function(){
+        (function(){
+            var a = [8,25,6,78,12];
+            map(a, function rest(value){
+                return value -1
+            });
+            
+            var result = [7,24,5,77,11];
+
+            assert(result.length === 5, 'The result length should be 3' );
+            assert(result[0] === 7, 'Index 0 should be 7');
+            assert(result[1] === 24, 'Index 0 should be 7');
+            assert(result[2] === 5, 'Index 0 should be 7');
+            assert(result[3] === 77, 'Index 0 should be 7');
+            assert(result[4] === 11, 'Index 0 should be 7');
+        })();
+    });
+
+    it('Should failed if the first argument is not an array', function(){
+        (function(){
+            var _error;
+            try {
+                function expression(value){
+                    return value*2
+                }
+                map('hello', expression)
+
+            }catch(error){
+                _error = error;
+            }
+            assert(_error instanceof Array, 'The error should be TypeError');
+            assert(_error.message === 'Hello is not an Array', 'Should fail with message: "Hello is not an Array"');
+
+        })
+    })
+
+    it('Should failed if the second argument is not an expression', function(){
+        (function(){
+            var_error;
+            try {
+                var a = [1,5,3,6];
+                map(a, 'hello');
+
+            } catch (error) {
+                _error = error;
+            }
+            assert(expression === undefined, 'The error should be TypeError');
+            assert(_error.message === 'Is necessary an expresion', 'Should fail with message: "It is necessary an expression"');
+        })
+    })
+});
