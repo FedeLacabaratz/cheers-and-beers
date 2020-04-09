8c0a395e04ff03c5a00c0f8abb32dbe9a234c628
Fede Lacabaratz <lacarq@gmail.com>
2020-04-09 14:35:42 +0200

    add deletion of unnecessary folders and content

--- stuff/react/landing/main.1.jsx
+++
@@ -1,9 +0,0 @@
-const title = <h1>hola mundo</h1>
-
-const names = ['John', 'Mary', 'Charles', 'Annita', 'Pepito', 'Grillo']
-
-const items = names.map(name => <li>{name}</li>)
-
-const list = <ul>{items}</ul>
-
-ReactDOM.render([title, list], document.getElementById('root'))
